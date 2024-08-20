7a3e7c1909bf368b86a15ad11d471a5b11d2f0e8
MichalGlowacki <mcglowack@gmail.com>
2024-08-20 12:07:46 +0200

    Initial commit, first files, base structure

---
+++ src/App.js
@@ -0,0 +1,77 @@
+const initialFriends = [
+    {
+        id: 118836,
+        name: 'Clark',
+        image: 'https://i.pravatar.cc/48?u=118836',
+        balance: -7,
+    },
+    {
+        id: 933372,
+        name: 'Sarah',
+        image: 'https://i.pravatar.cc/48?u=933372',
+        balance: 20,
+    },
+    {
+        id: 499476,
+        name: 'Anthony',
+        image: 'https://i.pravatar.cc/48?u=499476',
+        balance: 0,
+    },
+];
+
+export default function App() {
+    return (
+        <div className="app">
+            <div className="sidebar">
+                <FriendsList />
+            </div>
+        </div>
+    );
+}
+
+function FriendsList() {
+    const friends = initialFriends;
+    return (
+        <ul>
+            {friends.map((friend) => (
+                <Friend
+                    friend={friend}
+                    key={friend.id}
+                />
+            ))}
+        </ul>
+    );
+}
+
+function Friend({ friend }) {
+    return (
+        <li>
+            <img
+                src={friend.image}
+                alt={friend.name}
+            />
+            <h3>{friend.name}</h3>
+            {friend.balance < 0 && (
+                <p className="red">
+                    You owe {friend.name} {Math.abs(friend.balance)}$
+                </p>
+            )}
+            {friend.balance > 0 && (
+                <p className="green">
+                    {friend.name} owes You {Math.abs(friend.balance)}$
+                </p>
+            )}
+            {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+        </li>
+    );
+}
+
+function formAddFriend() {
+    return <form className="form-add-friend">
+        <label>Friend name</label>
+        <input type="text"></input>
+
+        <label>Image url</label>
+        <input type="text"></input>
+    </form>;
+}
